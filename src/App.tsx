83629f65e5969c9277eddf9ed2661abb001199e5
Hadeer Mahmoud <97056531+Hadeer-Mah@users.noreply.github.com>
2023-10-11 01:31:25 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState } from "react";
+import { Task } from "./interfaces";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [tasksList, setTasksList] = useState<Task[]>([]);
+  const [task, setTask] = useState<Task>({} as Task);
+  function addTaskHandler() {
+    setTasksList([...tasksList, task]);
+    setTask({name: ""} as Task);
+  }
+  function deleteTaskHandler(index: number) {
+    setTasksList(
+      tasksList.filter((task:Task, i) => {
+        return i !== index;
+      })
+    );
+  }
+  function doneTaskHandler(index:number) {
+    setTasksList(tasksList.map((task:Task, i:number)=>{
+      if (i === index) {
+        return { ...task, done: true };
+      }
+      return task;
+    }) );
+  }
+  console.log(tasksList)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contentContainer">
+        <div className="title">
+          <h1>Todo List</h1>
+          <p>A simple to-do list using React + Typescript</p>
+        </div>
+        <div className="tasksContainer">
+          {tasksList.map((task:Task, index:number) => {
+            return (
+              <div className="checkTask" key={index}>
+                <label
+                  className={task.done ? "done" : ""}
+                >
+                  {task.name}
+                </label>
+                {!task.done && ( <i
+                  className="fa-regular fa-square-check"
+                  onClick={() => {
+                    doneTaskHandler(index)
+                  }}
+                ></i> )}
+                
+                <i
+                  className="fa-solid fa-trash delete"
+                  onClick={() => {
+                    deleteTaskHandler(index);
+                  }}
+                ></i>
+              </div>
+            );
+          })}
+        </div>
+        <div className="addingTask">
+          <input
+            type="text"
+            value={task.name || ""}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setTask({name: e.target.value, done: false});
+            }}
+          />
+          <button onClick={addTaskHandler}>Add Task</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
